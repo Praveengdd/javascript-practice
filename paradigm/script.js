64a110556f9658d17f036828b7c1c64eccee6308
Praveengdd <jayasheelagd6@gmail.com>
2025-09-29 12:14:51 +0530

    Learned different types of paradigms

---
+++ paradigm/script.js
@@ -0,0 +1,87 @@
+//Paradigm - the way or style of programming
+
+//procedural programming
+
+// function consoleEnd() {
+//     console.log("Program is over")
+// }
+
+// function zero() {
+//     console.log("Number entered is zero");
+// }
+
+// function checkNumber(number) {
+//     if(number === 0) zero();
+//     else if(number >= 0) positive();
+//     else negative();
+
+//     consoleEnd();
+
+// }
+
+// function positive() {
+//     console.log("Number is positive");
+// }
+
+// function negative() {
+//     console.log("Number is negative");
+// }
+
+
+// checkNumber(5);
+// checkNumber(-10);
+// checkNumber(0);
+
+
+//Structured programming
+
+// function checkNumber(num) {
+//     if(num === 0) {
+//         console.log("Number entered is zero");
+//     }
+//     else if(num > 0) {
+//         console.log("Number is positive");
+//     }
+//     else {
+//         console.log("Number is negative");
+//     }
+//     console.log("Program is over");
+// }
+
+
+// checkNumber(10);
+// checkNumber(-5);
+// checkNumber(0);
+
+
+//Object Oriented Programming
+
+// class Car{
+//     name;
+//     company;
+
+//     constructor(name, company) {
+//         this.name = name;
+//         this.company = company;
+//     }
+
+//     start() {
+//         console.log("Car has started and is ready to move");
+//     }
+
+//     stop() {
+//         console.log("Car has stopped!!!")
+//     }
+
+//     printDetails() {
+//         console.log(`${this.company} ${this.name}`);
+//     }
+// }
+
+
+// hyundai = new Car("Elantra", "Hyundai");
+// hyundai.start();
+// hyundai.stop();
+// hyundai.printDetails();
+
+// console.log(hyundai);
